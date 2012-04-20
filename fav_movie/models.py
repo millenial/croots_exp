6a81bc749d7132f196efbe22a0b427e24f66d2bd
octaveon <=>
2012-04-20 19:50:06 -0400

    updated stuff

--- fav_movie/models.py
+++ fav_movie/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Movies(models.Model):
+    favorite_movie = models.CharField(max_length=30)
+    userid = models.ForeignKey('userinfo.User')
+    def __unicode__(self):
+        return self.favorite_movie
--- fav_movie/views.py
+++ fav_movie/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+from django.template import Context, Template
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def listmovies(request):
+    t = Template('hello world')
+    return HttpResponse('hello world')
+
