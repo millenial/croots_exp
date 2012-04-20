08cb5964652474fd802e696203beed8cb4f8671d
y-series <y-series@ubuntu.ubuntu-domain>
2012-04-20 18:47:41 -0400

    working model and view

---
+++ userinfo/views.py
@@ -0,0 +1,7 @@
+# Create your views here.
+from django.http import HttpResponse
+from userinfo.models import User
+
+def main(request):
+    output = ', '.join([usr for usr in User.objects.all()])
+    return HttpResponse("User details : \n"+output)
