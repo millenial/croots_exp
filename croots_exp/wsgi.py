62938f6a9e3c73e0f5ce691b248f792abc4549a0
y-series <y-series@ubuntu.ubuntu-domain>
2012-04-20 16:13:44 -0400

    added wsgi . and .gitignore

---
+++ croots_exp/wsgi.py
@@ -0,0 +1,28 @@
+"""
+WSGI config for croots_exp project.
+
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
+
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
+
+"""
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "croots_exp.settings")
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+# Apply WSGI middleware here.
+# from croots_exp.wsgi import croots_expApplication
+# application = croots_expApplication(application)
